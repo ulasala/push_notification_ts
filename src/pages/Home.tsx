31453f7125f3ac8fcc30e414153f2f3acd06fc7b
ashok ulasala <ashokulasala@gmail.com>
2024-05-22 00:00:13 +0530

    code refactored

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import GridLayoutChat from '../components/GridLayout';
+import GridLayout from '../components/GridLayout';
 
 type Props = {};
 
 const Home = (props: Props) => {
   return (
     <div>
-      <GridLayoutChat />
+      <GridLayout />
     </div>
   );
 };
